a7f5fbafb024a16b9eeaab79a0b14c9f460f84d9
Yash-Vekaria <30694521+Yash-Vekaria@users.noreply.github.com>
2021-01-31 22:59:40 +0530

    Create find_site_leaning.py
    Adding code to obtain the political leaning on any website in our dataset.

---
+++ Cookie Synchronization/find_site_leaning.py
@@ -0,0 +1,96 @@
+# List of 123 Indian News Websites
+Url_list = ["https://aajtak.intoday.in/", "https://www.jagran.com/", "https://www.rvcj.com/", "http://bhaskar.com/",
+            "https://zeenews.india.com/", "https://www.indiatvnews.com/", "https://timesofindia.indiatimes.com/",
+            "https://www.indiatoday.in/", "https://news24online.com/", "https://news.abplive.com/",
+            "https://www.bbc.com/hindi/", "https://www.ndtv.com/", "https://www.amarujala.com/",
+            "https://www.indiatimes.com/", "https://navbharattimes.indiatimes.com/", "https://indianexpress.com/",
+            "https://www.thequint.com/", "https://www.patrika.com/", "https://www.news18.com/",
+            "https://thelogicalindian.com/", "https://www.hindustantimes.com/", "https://www.timesnownews.com/",
+            "https://www.punjabkesari.in/", "https://www.thehindu.com/", "https://www.anandabazar.com/",
+            "https://www.scoopwhoop.com/", "https://www.jansatta.com/", "https://economictimes.indiatimes.com/",
+            "https://khabar.ndtv.com/", "https://www.mid-day.com/", "https://www.dailythanthi.com/",
+            "http://newsonair.nic.in/", "http://sandesh.com/", "https://www.dinamalar.com/",
+            "https://www.mathrubhumi.com/", "https://www.newindianexpress.com/", "https://www.brut.media/in",
+            "https://www.india.com/", "https://www.firstpost.com/", "https://www.manoramaonline.com/",
+            "https://www.lokmat.com/", "https://www.livehindustan.com/", "https://www.thebetterindia.com/",
+            "https://www.greaterkashmir.com/", "https://www.republicworld.com/", "https://www.financialexpress.com/",
+            "https://www.newslivetv.com/", "https://www.pinkvilla.com/", "https://www.cnbctv18.com/",
+            "https://scroll.in/", "https://www.businesstoday.in/", "https://www.deccanchronicle.com/",
+            "https://www.siasat.com/", "http://www.fakingnews.com/", "https://www.dnaindia.com/",
+            "https://andhrajyothy.com/", "https://www.gujaratsamachar.com/", "https://m.dailyhunt.in/",
+            "https://www.newdelhitimes.com/", "https://kashmirlife.net/", "https://www.outlookindia.com/",
+            "https://www.livemint.com/", "http://www.catchnews.com/", "https://www.eenadu.net/",
+            "https://www.huffingtonpost.in/", "https://hindi.webdunia.com/", "https://www.thenewsminute.com/",
+            "https://www.moneycontrol.com/", "https://www.businessinsider.in/", "https://topyaps.com/",
+            "https://www.newsnationtv.com/", "https://thewire.in/", "https://www.deccanherald.com/",
+            "http://youthkiawaaz.com/", "https://www.thehindubusinessline.com/", "https://swarajyamag.com/",
+            "https://theprint.in/", "https://www.wionews.com/", "http://risingkashmir.com/", "https://www.aninews.in/",
+            "https://telanganatoday.com/", "https://www.dailyo.in/", "http://ptinews.com/",
+            "https://www.forbesindia.com/", "https://www.milligazette.com/", "http://dailyexcelsior.com/",
+            "https://www.business-standard.com/", "https://wisden.com/", "https://mumbaimirror.indiatimes.com/",
+            "https://www.thestatesman.com/", "https://news.statetimes.in/", "https://www.sentinelassam.com/",
+            "https://www.telegraphindia.com/", "https://www.tribuneindia.com/", "https://www.oneindia.com/",
+            "http://www.assamtribune.com/", "https://www.deshabhimani.com/", "https://www.newslaundry.com/",
+            "https://bangaloremirror.indiatimes.com/", "https://www.opindia.com/", "http://www.socialsamosa.com/",
+            "https://www.newstracklive.com/", "https://www.altnews.in/", "https://leagueofindia.com/",
+            "https://www.prabhatkhabar.com/", "https://www.thesangaiexpress.com/", "https://kashmirreader.com/",
+            "https://starofmysore.com/", "https://news4masses.com/", "https://www.headlinesoftoday.com/",
+            "https://countercurrents.org/", "https://ahmedabadmirror.indiatimes.com/",
+            "https://punemirror.indiatimes.com/", "https://www.nagpurtoday.in/", "https://www.sinceindependence.com/",
+            "https://www.dailypioneer.com/", "https://timesheadline.com/", "http://asianage.com/",
+            "https://www.freepressjournal.in/", "https://5abnow.com/", "https://www.greatandhra.com/",
+            "https://ians.in/", "https://www.factchecker.in/"]
+
+# Final Expert Labels
+expert_labels = ["RIGHT_TO_RIGHTCENTRE", "RIGHT_TO_RIGHTCENTRE", "#N/A", "RIGHT_TO_RIGHTCENTRE", "RIGHT_TO_RIGHTCENTRE",
+           "RIGHT_TO_RIGHTCENTRE", "CENTRIST_AND_LEASTBIASED", "CENTRIST_AND_LEASTBIASED", "CENTRIST_AND_LEASTBIASED",
+           "RIGHT_TO_RIGHTCENTRE", "CENTRIST_AND_LEASTBIASED", "LEFT_TO_LEFTCENTRE", "RIGHT_TO_RIGHTCENTRE",
+           "RIGHT_TO_RIGHTCENTRE", "RIGHT_TO_RIGHTCENTRE", "LEFT_TO_LEFTCENTRE", "CENTRIST_AND_LEASTBIASED",
+           "RIGHT_TO_RIGHTCENTRE", "RIGHT_TO_RIGHTCENTRE", "#N/A", "LEFT_TO_LEFTCENTRE", "RIGHT_TO_RIGHTCENTRE",
+           "LEFT_TO_LEFTCENTRE", "LEFT_TO_LEFTCENTRE", "LEFT_TO_LEFTCENTRE", "LEFT_TO_LEFTCENTRE",
+           "CENTRIST_AND_LEASTBIASED", "CENTRIST_AND_LEASTBIASED", "LEFT_TO_LEFTCENTRE", "CENTRIST_AND_LEASTBIASED",
+           "CENTRIST_AND_LEASTBIASED", "RIGHT_TO_RIGHTCENTRE", "LEFT_TO_LEFTCENTRE", "LEFT_TO_LEFTCENTRE",
+           "LEFT_TO_LEFTCENTRE", "RIGHT_TO_RIGHTCENTRE", "#N/A", "RIGHT_TO_RIGHTCENTRE", "RIGHT_TO_RIGHTCENTRE",
+           "CENTRIST_AND_LEASTBIASED", "LEFT_TO_LEFTCENTRE", "CENTRIST_AND_LEASTBIASED", "#N/A", "LEFT_TO_LEFTCENTRE",
+           "RIGHT_TO_RIGHTCENTRE", "CENTRIST_AND_LEASTBIASED", "RIGHT_TO_RIGHTCENTRE", "#N/A",
+           "CENTRIST_AND_LEASTBIASED", "LEFT_TO_LEFTCENTRE", "CENTRIST_AND_LEASTBIASED", "RIGHT_TO_RIGHTCENTRE",
+           "LEFT_TO_LEFTCENTRE", "RIGHT_TO_RIGHTCENTRE", "RIGHT_TO_RIGHTCENTRE", "LEFT_TO_LEFTCENTRE",
+           "RIGHT_TO_RIGHTCENTRE", "RIGHT_TO_RIGHTCENTRE", "RIGHT_TO_RIGHTCENTRE", "LEFT_TO_LEFTCENTRE",
+           "LEFT_TO_LEFTCENTRE", "CENTRIST_AND_LEASTBIASED", "LEFT_TO_LEFTCENTRE", "LEFT_TO_LEFTCENTRE",
+           "LEFT_TO_LEFTCENTRE", "RIGHT_TO_RIGHTCENTRE", "LEFT_TO_LEFTCENTRE", "RIGHT_TO_RIGHTCENTRE",
+           "CENTRIST_AND_LEASTBIASED", "#N/A", "RIGHT_TO_RIGHTCENTRE", "LEFT_TO_LEFTCENTRE", "CENTRIST_AND_LEASTBIASED",
+           "LEFT_TO_LEFTCENTRE", "LEFT_TO_LEFTCENTRE", "RIGHT_TO_RIGHTCENTRE", "CENTRIST_AND_LEASTBIASED",
+           "CENTRIST_AND_LEASTBIASED", "LEFT_TO_LEFTCENTRE", "RIGHT_TO_RIGHTCENTRE", "#N/A", "RIGHT_TO_RIGHTCENTRE",
+           "CENTRIST_AND_LEASTBIASED", "RIGHT_TO_RIGHTCENTRE", "LEFT_TO_LEFTCENTRE", "RIGHT_TO_RIGHTCENTRE",
+           "CENTRIST_AND_LEASTBIASED", "#N/A", "LEFT_TO_LEFTCENTRE", "LEFT_TO_LEFTCENTRE", "#N/A", "#N/A",
+           "LEFT_TO_LEFTCENTRE", "CENTRIST_AND_LEASTBIASED", "RIGHT_TO_RIGHTCENTRE", "#N/A", "LEFT_TO_LEFTCENTRE",
+           "LEFT_TO_LEFTCENTRE", "LEFT_TO_LEFTCENTRE", "RIGHT_TO_RIGHTCENTRE", "#N/A", "#N/A", "LEFT_TO_LEFTCENTRE",
+           "#N/A", "#N/A", "#N/A", "LEFT_TO_LEFTCENTRE", "RIGHT_TO_RIGHTCENTRE", "#N/A", "CENTRIST_AND_LEASTBIASED",
+           "LEFT_TO_LEFTCENTRE", "LEFT_TO_LEFTCENTRE", "LEFT_TO_LEFTCENTRE", "CENTRIST_AND_LEASTBIASED", "#N/A",
+           "RIGHT_TO_RIGHTCENTRE", "LEFT_TO_LEFTCENTRE", "CENTRIST_AND_LEASTBIASED", "CENTRIST_AND_LEASTBIASED", "#N/A",
+           "LEFT_TO_LEFTCENTRE", "RIGHT_TO_RIGHTCENTRE", "#N/A"]
+
+
+# Code to assign shorter political labels to each site
+def simplify_labels(labels):
+    for indx in range(len(Url_list)):
+        leaning = expert1[indx]
+        website = Url_list[indx]
+        if "LEFT" in leaning:
+            labels[website] = "LEFT"
+        elif "RIGHT" in leaning:
+            labels[website] = "RIGHT"
+        elif "CENTRIST" in leaning:
+            labels[website] = "CENTRE"
+        else:
+            labels[website] = "NA"
+    return labels
+
+# Code to return the political leaning of input website
+def get_leaning(website):
+    labels = simplify_labels({})
+    return labels[website]
+
+# Code to obtain the website based on the index
+def get_website_from_index(indx):
+    return Url_list[indx]
